057e58d806d22e1d8d23430870ad27158fa4a1d2
Cole Howard <uglyboxer@gmail.com>
2018-07-26 14:33:59 -0700

    missed agent

--- agent.py
+++ agent.py
@@ -182,17 +182,17 @@ class Agent():
             self.train_value_loss.append(round(fit.history['value_head_loss'][config.EPOCHS - 1], 4))
             self.train_policy_loss.append(round(fit.history['policy_head_loss'][config.EPOCHS - 1], 4))
 
-        plt.plot(self.train_overall_loss, 'k')
-        plt.plot(self.train_value_loss, 'k:')
-        plt.plot(self.train_policy_loss, 'k--')
+        # plt.plot(self.train_overall_loss, 'k')
+        # plt.plot(self.train_value_loss, 'k:')
+        # plt.plot(self.train_policy_loss, 'k--')
 
-        plt.legend(['train_overall_loss', 'train_value_loss', 'train_policy_loss'], loc='lower left')
+        # plt.legend(['train_overall_loss', 'train_value_loss', 'train_policy_loss'], loc='lower left')
 
-        display.clear_output(wait=True)
-        display.display(pl.gcf())
-        pl.gcf().clear()
+        # display.clear_output(wait=True)
+        # display.display(pl.gcf())
+        # pl.gcf().clear()
         time.sleep(1.0)
-        
+
         self.model.printWeightAverages()
 
     def predict(self, inputToModel):
